0e3fa95789b326343183c86f7fcf0e0a847ff8c1
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-12-07 19:38:15 +0300

    Save raw audio to file

--- app/src/main/java/ru/fixapp/fooproject/domainlayer/FileInfoConverter.java
+++ app/src/main/java/ru/fixapp/fooproject/domainlayer/FileInfoConverter.java
@@ -1,8 +1,6 @@
 package ru.fixapp.fooproject.domainlayer;
 
 import android.content.Context;
-import android.media.MediaMetadataRetriever;
-import android.net.Uri;
 
 import java.io.File;
 
@@ -22,18 +20,18 @@ public class FileInfoConverter {
 
 		String type;
 		long duration;
-		if (file.exists()) {
-			MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-			Uri uri = Uri.fromFile(file);
-			retriever.setDataSource(context, uri);
-			type = retriever
-					.extractMetadata(MediaMetadataRetriever.METADATA_KEY_MIMETYPE);
-			duration = Long.parseLong(retriever
-					.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
-		} else {
+//		if (file.exists()) {
+//			MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+//			Uri uri = Uri.fromFile(file);
+//			retriever.setDataSource(context, uri);
+//			type = retriever
+//					.extractMetadata(MediaMetadataRetriever.METADATA_KEY_MIMETYPE);
+//			duration = Long.parseLong(retriever
+//					.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+//		} else {
 			type = "";
 			duration = 0;
-		}
+//		}
 		long size = file.length();
 
 		return new AudioModel(file.getName(), type, size, duration, file.getAbsolutePath());
--- app/src/main/java/ru/fixapp/fooproject/domainlayer/interactors/AudioTrackRecorderInteractor.java
+++ app/src/main/java/ru/fixapp/fooproject/domainlayer/interactors/AudioTrackRecorderInteractor.java
@@ -3,13 +3,21 @@ package ru.fixapp.fooproject.domainlayer.interactors;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import net.jokubasdargis.rxbus.Bus;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import ru.fixapp.fooproject.presentationlayer.models.AudioEvents;
 import ru.fixapp.fooproject.presentationlayer.models.QueriesBus;
 import rx.Observable;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 import static android.media.AudioRecord.RECORDSTATE_RECORDING;
@@ -45,16 +53,44 @@ public class AudioTrackRecorderInteractor implements IAudioRecorderInteractor {
 
 	@Override
 	public Observable<Void> start(String path) {
-//		recorder.setOutputFile(path);
 
+		return Observable.combineLatest(getAudioRecordObservable(),
+				getFileStreamObservable(path),(container, dataOutputStream) -> saveToFile(dataOutputStream, container)).ignoreElements().map(r -> null);
+	}
+
+	@Nullable
+	private Void saveToFile(DataOutputStream stream, Container container) {
+		try {
+			short[] audioBuffer = container.audioBuffer;
+			Log.e("Audio", "start writeShort " + audioBuffer.length);
+			for (int i = 0; i < audioBuffer.length; i++) {
+				stream.writeShort(audioBuffer[i]); //Save each number
+			}
+			Log.e("Audio", "End writeShort " + audioBuffer.length);
+
+		} catch (IOException e) {
+			Log.e("Audio", "File write failed: " + e.toString());
+		}
+		return null;
+	}
+
+	private Observable<DataOutputStream> getFileStreamObservable(String path) {
+		return Observable.fromCallable(() -> new DataOutputStream(
+				new BufferedOutputStream(new FileOutputStream(new File(path)))))
+				.subscribeOn(Schedulers.io());
+	}
+
+	private Observable<Container> getAudioRecordObservable() {
 		return Observable.fromCallable(() -> {
 			Log.d("Audio", "state " + record.getState());
 			record.startRecording();
 			return null;
 		})
 				.observeOn(Schedulers.newThread())
-				.concatMap(o ->
-						Observable.create(subscriber -> {
+				.concatMap(new Func1<Object, Observable<Container>>() {
+					@Override
+					public Observable<Container> call(Object o) {
+						return Observable.create(subscriber -> {
 							long shortsRead = 0;
 							short[] audioBuffer = new short[bufferSize / 2];
 							Log.d("Audio", "start Read");
@@ -62,12 +98,17 @@ public class AudioTrackRecorderInteractor implements IAudioRecorderInteractor {
 								int numberOfShort = record.read(audioBuffer, 0, audioBuffer
 										.length);
 								shortsRead += numberOfShort;
+								subscriber.onNext(new Container(audioBuffer));
 								Log.d("Audio", "read =" + shortsRead);
 							}
 							Log.d("Audio", "stop Read");
 							if (!subscriber.isUnsubscribed())
 								subscriber.onCompleted();
-						}));
+						});
+					}
+				})
+				.onBackpressureBuffer()
+				.observeOn(Schedulers.io());
 	}
 
 	@Override
@@ -79,5 +120,13 @@ public class AudioTrackRecorderInteractor implements IAudioRecorderInteractor {
 			bus.publish(QueriesBus.AUDIO_EVENTS_QUEUE, AudioEvents.RECORDED);
 			return null;
 		});
+	}
+
+	private static class Container {
+		private final short[] audioBuffer;
+
+		public Container(short[] audioBuffer) {
+			this.audioBuffer = audioBuffer;
+		}
 	}
 }
